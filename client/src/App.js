da7e93739d62242d77eec9c22b7f4a9dfd0b2d6a
surajshende247 <surajshende247@gmail.com>
2023-11-08 09:03:54 +0530

    Add link display functionality to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css'
 import ImgCopy from "./copy.png"
 import axios from 'axios'
@@ -7,6 +7,7 @@ function App() {
   const [url, setUrl] = useState('')
   const [slug, setSlug] = useState('')
   const [shortUrl, setShortUrl] = useState('')
+  const [links, setLinks] = useState([]);
 
   const  generateLink = async () => {
     const response = await axios.post('/link', {
@@ -21,6 +22,16 @@ function App() {
     navigator.clipboard.writeText(shortUrl);
     alert('Copied to clipboard!')
   }
+
+  const loadLinks = async () => {
+    const response = await axios.get('/api/links');
+
+    setLinks(response?.data?.data)
+  }
+
+  useEffect(()=>{
+    loadLinks();
+  }, [])
 
   return (
     <div>
@@ -72,8 +83,20 @@ function App() {
 
         </div>
 
-        <div>
-            <h2>All Links</h2>
+        <div className='all-links-container'>
+            {
+              links?.map((linkObj, index)=>{
+                const {url, slug, clicks} = linkObj;
+
+                return (
+                  <div className="link-card" key={index}>
+                    <p>URL: {url}</p>
+                    <p>Short URL: {process.env.REACT_APP_BASE_URL}/{slug}</p>
+                    <p>Clicks: {clicks}</p>
+                  </div>
+                )
+              })
+            }
         </div>
       </div>
     </div>
--- server/index.js
+++ server/index.js
@@ -65,6 +65,16 @@ app.get("/:slug", async (req, res)=>{
   res.redirect(link.url);
 })
 
+app.get("/api/links", async (req, res)=>{
+  const links = await Link.find({});
+
+  return res.json({
+    success: true,
+    data: links,
+    message: "Links fetched successfully"
+  })
+})
+
 
 const PORT = process.env.PORT || 5000;
 
